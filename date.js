15f9e0ea6684bcda304076054885b357d8989267
Kurtis <kurtis@edge.app>
2023-03-07 23:20:38 -0800

    Add functions for clock ticker functionality

--- date.js
+++ date.js
@@ -1,9 +1,42 @@
-export const currentTime = new Date()
-export const body = document.body
+const currentTime = new Date()
+const body = document.body
 
 console.log(currentTime)
 
+// Add current time to the clock
+
+const hour = currentTime.getHours()
+const minutes = currentTime.getMinutes()
+const seconds = currentTime.getSeconds()
+
+console.log(hour, minutes, seconds)
+
+const ticker = document.createElement('ticker')
+ticker.id = 'ticker'
+ticker.innerHTML = '<div> : </div>'
+body.append(ticker)
+
+const hiddenTicker = () => {
+  document.getElementById('ticker').style.visibility = 'hidden'
+}
+
+const visibleTicker = () => {
+  document.getElementById('ticker').style.visibility = 'visible'
+}
+
+// Time is offset by one second allow the colon to be hidden and then visible again and look natural
+const hiddenInterval = setInterval(hiddenTicker, 1000)
+const visibleInterval = setInterval(visibleTicker, 2000)
+
+currentTimeFormat = `${hour} 
+  ${minutes}  ${seconds}`
+const currentTimeElement = document.createElement('div')
+currentTimeElement.innerHTML = `<div> ${currentTimeFormat} </div>`
+body.append(currentTimeElement)
+
 // month and day of week are zero based
+
+// Add the current date
 
 const months = [
   'January',
--- time.js
+++
@@ -1,8 +0,0 @@
-import { currentTime, body } from './date'
-
-const hour = currentTime.getHours()
-const minutes = currentTime.getMinutes()
-const seconds = currentTime.getSeconds()
-
-console.log(hour, minutes, seconds)
-// const clockTick = () => {}
