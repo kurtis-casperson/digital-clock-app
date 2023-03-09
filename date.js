e2a9c5348cdd26665e6bc4f2ceeca9fe2ab2209c
Kurtis <kurtis@edge.app>
2023-03-08 22:39:49 -0800

    Add ticker function to clock.  Add Nth number to date

--- date.js
+++ date.js
@@ -1,38 +1,7 @@
-const currentTime = new Date()
-const body = document.body
+export const currentTime = new Date()
+export const body = document.body
 
 console.log(currentTime)
-
-// Add current time to the clock
-
-const hour = currentTime.getHours()
-const minutes = currentTime.getMinutes()
-const seconds = currentTime.getSeconds()
-
-console.log(hour, minutes, seconds)
-
-const ticker = document.createElement('ticker')
-ticker.id = 'ticker'
-ticker.innerHTML = '<div> : </div>'
-body.append(ticker)
-
-const hiddenTicker = () => {
-  document.getElementById('ticker').style.visibility = 'hidden'
-}
-
-const visibleTicker = () => {
-  document.getElementById('ticker').style.visibility = 'visible'
-}
-
-// Time is offset by one second allow the colon to be hidden and then visible again and look natural
-const hiddenInterval = setInterval(hiddenTicker, 1000)
-const visibleInterval = setInterval(visibleTicker, 2000)
-
-currentTimeFormat = `${hour} 
-  ${minutes}  ${seconds}`
-const currentTimeElement = document.createElement('div')
-currentTimeElement.innerHTML = `<div> ${currentTimeFormat} </div>`
-body.append(currentTimeElement)
 
 // month and day of week are zero based
 
@@ -74,6 +43,16 @@ const currentMonth = months[month]
 const dayOfMonth = currentTime.getDate()
 
 const year = currentTime.getFullYear()
+
+const nthDayofMonth = (nthnumber) => {
+  if (nthnumber > 3 && nthnumber < 21) return 'th'
+}
+
+const stDaysOfMonth = [1, 21, 31]
+
+const rdDaysOfMonth = []
+
+const thDaysOfMonth = []
 
 const currentDateFormat = `${currentDayOfWeek}, ${currentMonth} ${dayOfMonth} ${year}`
 
---
+++ time.js
@@ -0,0 +1,39 @@
+import { currentTime, body } from './date.js'
+// Add current time to the clock
+
+const hour = currentTime.getHours()
+const minutes = currentTime.getMinutes()
+const seconds = currentTime.getSeconds()
+
+console.log(hour, minutes, seconds)
+
+const ticker = document.createElement('ticker')
+ticker.id = 'ticker'
+ticker.innerHTML = '<div> : </div>'
+body.append(ticker)
+
+const hiddenTicker = () => {
+  document.getElementById('ticker').style.visibility = 'hidden'
+}
+
+const visibleTicker = () => {
+  document.getElementById('ticker').style.visibility = 'visible'
+}
+
+// const decideTicker = () => {
+//   if (ticker === visibleTicker) {
+//     hiddenTicker
+//   } else {
+//     return hiddenTicker
+//   }
+// }
+
+// Time is offset by one second allow the colon to be hidden and then visible again and look natural
+const hiddenInterval = setInterval(hiddenTicker, 1000)
+const visibleInterval = setInterval(visibleTicker, 2000)
+
+const currentTimeFormat = `${hour} 
+  ${minutes}  ${seconds}`
+const currentTimeElement = document.createElement('div')
+currentTimeElement.innerHTML = `<div> ${currentTimeFormat} </div>`
+body.append(currentTimeElement)
