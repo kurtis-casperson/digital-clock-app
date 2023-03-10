313f8763d1ef43ed6ffd787b12bb460d4982b064
Kurtis <kurtis@edge.app>
2023-03-09 18:46:43 -0800

    Add

--- time.js
+++ time.js
@@ -5,21 +5,17 @@ const hour = currentTime.getHours()
 const minutes = currentTime.getMinutes()
 const seconds = currentTime.getSeconds()
 
-console.log(hour, minutes, seconds)
+const hourStandard = hour > 12 ? hour - 12 : hour
+const addZero = hour >= 13 && hour <= 21 ? '0' : ''
+const amPm = hour > 12 ? 'PM' : 'AM'
 
-// Should this be its own element?
-const ticker = document.createElement('ticker')
-ticker.id = 'ticker'
-ticker.innerHTML = '<div> : </div>'
-body.append(ticker)
+// const hiddenTicker = () => {
+//   document.getElementById('seconds').style.visibility = 'hidden'
+// }
 
-const hiddenTicker = () => {
-  document.getElementById('ticker').style.visibility = 'hidden'
-}
-
-const visibleTicker = () => {
-  document.getElementById('ticker').style.visibility = 'visible'
-}
+// const visibleTicker = () => {
+//   document.getElementById('seconds').style.visibility = 'visible'
+// }
 
 // const decideTicker = () => {
 //   if (ticker === visibleTicker) {
@@ -30,11 +26,11 @@ const visibleTicker = () => {
 // }
 
 // Time is offset by one second allow the colon to be hidden and then visible again and look natural
-const hiddenInterval = setInterval(hiddenTicker, 1000)
-const visibleInterval = setInterval(visibleTicker, 2000)
+// const hiddenInterval = setInterval(hiddenTicker, 1000)
+// const visibleInterval = setInterval(visibleTicker, 2000)
 
-const currentTimeFormat = `${hour} 
-  ${minutes}  ${seconds}`
+const currentTimeFormat = `${addZero}${hourStandard} :
+  ${minutes} : ${seconds} ${amPm}`
 const currentTimeElement = document.createElement('div')
 currentTimeElement.id = 'currentTimeElement'
 currentTimeElement.innerHTML = `<div> ${currentTimeFormat} </div>`
